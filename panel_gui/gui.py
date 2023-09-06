87cfe0fa0c7e7735306993649dd66753c26a2785
ruan <ruan.vantonder@vastech.com>
2023-09-06 21:55:22 +0200

    Add simulation comparison to gui

--- panel_gui/gui.py
+++ panel_gui/gui.py
@@ -4,14 +4,21 @@ from simulation_templates import (
     SimulatorTemplate,
     get_simulators
 )
-from panel_gui.plotting import plot_single_trace
+from panel_gui.plotting import plot_single_trace,plot_multiple_traces
 
 simulation_results = []
 simulation_display = pn.pane.Plotly()
+stacked_display = pn.pane.Plotly()
 
 def update_display():
     if len(simulation_results) >= 1:
         simulation_display.object = plot_single_trace(simulation_results[-1][0],simulation_results[-1][1])
+    
+    if len(simulation_results) >= 2:
+        unzipped = zip(*simulation_results)
+        names = next(unzipped)
+        traces = next(unzipped)
+        stacked_display.object = plot_multiple_traces(names,traces)
 
 def run_simulation(name,arguments,function):
     simulation_results.append((name,function(**arguments)))
@@ -33,8 +40,9 @@ def create_simulator_pane(simulator : SimulatorTemplate):
     return simulator.name,pane
 tabs = [tuple(create_simulator_pane(sim)) for sim in get_simulators()]
 tabs = pn.Tabs(*tabs)
+plots = pn.Column(simulation_display,stacked_display)
 main_row = pn.Row(
-    tabs,simulation_display
+    tabs,plots
 )
 main_row.servable()
 
--- panel_gui/plotting.py
+++ panel_gui/plotting.py
@@ -1,65 +1,91 @@
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
+import plotly.express as px
 from rocket_architectures import Traces
 import pandas as pd
 import numpy as np
 from rocket_architectures import sim1
 
 
-def plot_single_trace(test_name : str,traces : Traces) -> go.Figure:
+def create_scatter_plots_from_trace(name, trace: Traces):
+    pos = go.Scatter(
+        x=trace.position[:, 0],
+        y=trace.position[:, 1],
+        name=name + " position",
+    )
+
+    speed = go.Scatter(
+        x=trace.time,
+        y=np.linalg.norm(trace.velocity, axis=1),
+        name=name + " speed",
+    )
+
+    acc = go.Scatter(
+        x=trace.time,
+        y=np.linalg.norm(trace.acceleration, axis=1) / 9.8,
+        name=name + " acceleration",
+    )
+    return pos, speed, acc
+
+
+def plot_single_trace(test_name: str, trace: Traces) -> go.Figure:
     fig = make_subplots(
-        rows = 2, 
-        cols = 1,
-        subplot_titles=("Position","Speed/Acceleration"),
-        specs=[[{}],[{"secondary_y" : True}]],
+        rows=2,
+        cols=1,
+        subplot_titles=("Position", "Speed/Acceleration"),
+        specs=[[{}], [{"secondary_y": True}]],
     )
+    pos, speed, acc = create_scatter_plots_from_trace(test_name, trace)
+
+    fig.add_trace(pos, row=1, col=1)
+
+    fig.add_trace(speed, row=2, col=1, secondary_y=False)
 
     fig.add_trace(
-        go.Scatter(
-            x = traces.position[:,0],
-            y = traces.position[:,1],
-            name = "position",
-        ),
-        row = 1,
-        col = 1
-    )
-
-    fig.add_trace(
-        go.Scatter(
-            x = traces.time,
-            y = np.linalg.norm(traces.velocity,axis=1),
-            name = "speed",
-        ),
-        row = 2,
-        col = 1,
-        secondary_y = False
-    )
-
-    fig.add_trace(
-        go.Scatter(
-            x = traces.time,
-            y = np.linalg.norm(traces.acceleration,axis=1)/9.8,
-            name = "acceleration",
-        ),
-        row = 2,
-        col = 1,
+        acc,
+        row=2,
+        col=1,
         secondary_y=True,
     )
 
-    fig.update_xaxes(title_text = "Horizontal Distance (m)",row=1,col=1)
-    fig.update_yaxes(title_text = "Altitude (m)",row=1,col=1)
+    fig.update_xaxes(title_text="Horizontal Distance (m)", row=1, col=1)
+    fig.update_yaxes(title_text="Altitude (m)", row=1, col=1)
 
-    fig.update_xaxes(title_text="Time",row=2,col=1)
-    fig.update_yaxes(title="speed (m/s)",secondary_y = False,row=2,col=1)
-    fig.update_yaxes(title="acceleration (g)",secondary_y = True,row=2,col=1)
+    fig.update_xaxes(title_text="Time", row=2, col=1)
+    fig.update_yaxes(title="speed (m/s)", secondary_y=False, row=2, col=1)
+    fig.update_yaxes(title="acceleration (g)", secondary_y=True, row=2, col=1)
 
     return fig
 
+
+def plot_multiple_traces(test_names: list[str], traces: list[Traces]) -> go.Figure:
+    fig = make_subplots(
+        rows=3,
+        cols=1,
+        subplot_titles=("Position", "Speed", "Acceleration"),
+    )
+
+    for test_number,(name, trace) in enumerate(zip(test_names, traces)):
+        scatters = create_scatter_plots_from_trace(name, trace)
+        for i, scatter in enumerate(scatters):
+            fig.add_trace(scatter, row=i+1, col=1)
+            fig.data[-1].update(marker=dict(color=px.colors.qualitative.Plotly[test_number]))
+
+    fig.update_xaxes(title_text="Horizontal Distance (m)", row=1, col=1)
+    fig.update_yaxes(title_text="Altitude (m)", row=1, col=1)
+
+    fig.update_xaxes(title_text="Time", row=2, col=1)
+    fig.update_yaxes(title="speed (m/s)", row=2, col=1)
+    fig.update_yaxes(title="acceleration (g)", row=3, col=1)
+
+    return fig
 
 
 if __name__ == "__main__":
     n = 100
     test_trace = sim1()
 
-    fig = plot_single_trace("sim 1",test_trace)
+    fig = plot_single_trace("sim 1", test_trace)
     fig.show()
+
+    plot_multiple_traces(["sim 1"]*2,[test_trace]*2).show()
