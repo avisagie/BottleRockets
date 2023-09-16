fd35df5c8419c4c9ea8352393ae76f22f2d84013
ruan <ruan.vantonder@vastech.com>
2023-09-16 18:18:07 +0200

    Display exceptions to user

--- panel_gui/gui.py
+++ panel_gui/gui.py
@@ -4,6 +4,8 @@ if not __name__.startswith("bokeh_app"):
 # add the root directory to the python path
 import sys
 from pathlib import Path
+from icecream import ic
+ic.disable()
 
 parent_dir = Path(".").parent.absolute()
 sys.path.append(str(parent_dir))
@@ -17,6 +19,7 @@ pn.extension("jsoneditor")
 
 simulation_results = []
 simulation_display = pn.pane.Plotly(sizing_mode="stretch_both")
+error_display = pn.pane.Alert("Ready",alert_type = "success")
 
 
 def update_display():
@@ -34,8 +37,15 @@ def update_display():
 
 
 def run_simulation(name, arguments, function):
-    simulation_results.append((name, function(**arguments)))
-    update_display()
+    try :
+        simulation_results.append((name, function(**arguments)))
+    except Exception as e:
+        error_display.alert_type = "danger"
+        error_display.object = str(e)
+    else:
+        error_display.alert_type = "success"
+        error_display.object = "done"
+        update_display()
 
 
 def create_simulator_pane(simulator: SimulatorTemplate):
@@ -50,5 +60,5 @@ def create_simulator_pane(simulator: SimulatorTemplate):
 
 tabs = [tuple(create_simulator_pane(sim)) for sim in get_simulators()]
 tabs = pn.Tabs(*tabs)
-main_row = pn.Row(tabs, simulation_display)
+main_row = pn.Row(tabs, pn.Column(error_display,simulation_display))
 main_row.servable()
