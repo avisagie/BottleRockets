44d678a08c3c171d14cdc7b3cbc4b5815a6e8863
ruan <ruan.vantonder@vastech.co.za>
2024-03-02 15:21:55 +0200

    Part of same rename as prev 2 commits

--- panel_gui/simulation_templates.py
+++ panel_gui/simulation_templates.py
@@ -1,6 +1,7 @@
-import rocket_architectures as ra
-from collections import namedtuple
 import inspect
+from collections import namedtuple
+
+import rocket_architectures as ra
 
 SimulatorTemplate = namedtuple("simulator", ["name", "arguments", "function"])
 
@@ -8,13 +9,12 @@ SimulatorTemplate = namedtuple("simulator", ["name", "arguments", "function"])
 def get_simulators() -> list[SimulatorTemplate]:
     simulators_functions = [
         ra.sim_single_bottle,
-        ra.sim_3_boosters,
+        ra.sim_rocket_with_boosters,
         ra.sim_3_boosters_bullet,
         ra.sim_3_stage,
     ]
     simulators = [
-        SimulatorTemplate(sim.__name__, get_signature(sim), sim)
-        for sim in simulators_functions
+        SimulatorTemplate(sim.__name__, get_signature(sim), sim) for sim in simulators_functions
     ]
     return simulators
 
